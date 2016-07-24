e990a66c55bbb0875a6a45d214701490124657ec
edwardlorilla <edwardlorilla123@gmail.com>
2016-07-25 00:49:58 +0800

    Add contacts

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -71,27 +71,6 @@
         </div>
     </div>
     @include('partials.contact')
-    <div id="modal">
-        <div id="heading" class="heading-color">
-            For more info send me an email
-        </div>
 
-        <div id="content">
-            <div class="txt-subject">
-                <p style="margin-left:10px;">Subject: </p></div>
-            <div class="content-subject">
-                <input type="text"/></div>
-            <div class="txt-email">
-                <p style="margin-left:10px;">Email: </p></div>
-            <div class="content-email">
-                <p id="email" style=" color:#464747; font:12px;"></p></div>
-            <div class="txt-message"><p>Message: </p></div>
-            <div class="content-message">
-                <textarea style="width:100%;background-color:#f7fbfe; margin-left:10px; height:100px;"></textarea></div>
-            <div class="contact-img"><img src="images/email.png" class="img-contact" alt=""/></div>
-
-            <div style="margin: 0 0 0 10px;"><a href="#" class="button blue position">Send</a></div>
-        </div>
-    </div>
 @endsection
 
--- resources/views/master/layout.blade.php
+++ resources/views/master/layout.blade.php
@@ -278,6 +278,28 @@ desired effect
         <section class="content">
 
             @yield('content')
+            <div id="modal">
+                <div id="heading" class="heading-color">
+                    For more info send me an email
+                </div>
+
+                <div id="content">
+                    <div class="txt-subject">
+                        <p style="margin-left:10px;">Subject: </p></div>
+                    <div class="content-subject">
+                        <input type="text"/></div>
+                    <div class="txt-email">
+                        <p style="margin-left:10px;">Email: </p></div>
+                    <div class="content-email">
+                        <p id="email" style=" color:#464747; font:12px;"></p></div>
+                    <div class="txt-message"><p>Message: </p></div>
+                    <div class="content-message">
+                        <textarea style="width:100%;background-color:#f7fbfe; margin-left:10px; height:100px;"></textarea></div>
+                    <div class="contact-img"><img src="images/email.png" class="img-contact" alt=""/></div>
+
+                    <div style="margin: 0 0 0 10px;"><a href="#" class="button blue position">Send</a></div>
+                </div>
+            </div>
 
         </section><!-- /.content -->
     </div><!-- /.content-wrapper -->
@@ -360,10 +382,51 @@ desired effect
 {!! Html::script('plugins/jQuery/jQuery-2.1.4.min.js') !!}
 {!! Html::script('js/bootstrap/js/bootstrap.min.js.js') !!}
 {!! Html::script('dist/js/app.min.js') !!}
-
-<!-- Optionally, you can add Slimscroll and FastClick plugins.
-     Both of these plugins are recommended to enhance the
-     user experience. Slimscroll is required when using the
-     fixed layout. -->
+{!! Html::script('js/script.js') !!}
+<script type="text/javascript">
+    var sorter = new TINY.table.sorter('sorter','table',{
+        headclass:'head',
+        ascclass:'asc',
+        descclass:'desc',
+        evenclass:'evenrow',
+        oddclass:'oddrow',
+        evenselclass:'evenselected',
+        oddselclass:'oddselected',
+        paginate:true,
+        size:10,
+        colddid:'columns',
+        currentid:'currentpage',
+        totalid:'totalpages',
+        startingrecid:'startrecord',
+        endingrecid:'endrecord',
+        totalrecid:'totalrecords',
+        hoverid:'selectedrow',
+        pageddid:'pagedropdown',
+        navid:'tablenav',
+        sortcolumn:1,
+        sortdir:1,
+        columns:[{index:7, format:'%', decimals:1},{index:8, format:'$', decimals:0}],
+        init:true
+    });
+</script>
+<!--Script Open Pannel--->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+{!! Html::script('js/jquery.reveal.js') !!}
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('.button-email').click(function (e) { // Button which will activate our modal
+            var title = $(this).attr('title');
+            var title2 = $('.name').attr('title');
+            document.getElementById("email").innerHTML = title.toString();
+            $('#modal').reveal({ // The item which will be opened with reveal
+                animation: 'fade',                   // fade, fadeAndPop, none
+                animationspeed: 600,                       // how fast animtions are
+                closeonbackgroundclick: true,              // if you click background will modal close?
+                dismissmodalclass: 'close'    // the class of a button or element that will close an open modal
+            });
+            return false;
+        });
+    });
+</script>
 </body>
 </html>
